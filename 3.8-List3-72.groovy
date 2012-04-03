dbab01bb31511e94491187808dc31706969b8dbc
Yuta Niimura <lastarpeggio@gmail.com>
2012-04-03 15:55:41 +0900

    upload

---
+++ 3.8-List3-72.groovy
@@ -0,0 +1,14 @@
+def (Integer a,Integer b) = [1,2]
+assert a == 1
+assert b == 2
+def (int c,double d,String e) = [4,5.5,"abc"]
+assert c == 4
+assert d == 5.5
+assert e == "abc"
+def ( f,g,h ) = [4,5.5,"abc"]
+assert f == 4
+assert g == 5.5
+assert h == "abc"
+(int i,double j,String k) = [4,5.5,"abc"]	//‚±‚ê‚ÍƒGƒ‰[
+
+//def‚ğg‚Á‚½—á
